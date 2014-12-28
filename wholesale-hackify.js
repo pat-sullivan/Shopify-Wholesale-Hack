09896004f7ecf540f7e45284df323ce125a626ad
pat-sullivan <pat@optiprintdesign.com>
2014-12-27 23:46:31 -0500

    shopify-wholesale-hackify_v-1
    Original Javascript code for Shopify wholesale-hack using jQuery and
    Liquid.

---
+++ wholesale-hackify.js
@@ -0,0 +1,26 @@
+  <script>
+      $(function() {
+        $wholesalehide = $(".product > div:contains('Wholesale')").closest(".product").detach(); // Create jQuery object and remove wholesale items for all users. (NOTE: Exact DOM path varies by site/theme)
+        $(".browse-tags").css("display", "none"); // Optional
+        
+        {% if customer.tags contains 'Wholesale' %}
+            $("#collection > .products").append($wholesalehide); // Append wholesale items into the "products" row for logged in wholesale customers.
+          var url = window.location.href;
+             if (url.search("collections/all") >= 0) { // Will only execute the following code if the user is on the main collection page
+               $('.title:contains("Chainmail Dishcloth for Your Finest Pots and Pans (Wholesale)")').closest(".product").addClass("clearit");
+           }; // The above code adds a clearing class to the parent product div that matches the text string in "contains"
+        {% endif %} 
+        
+       var url = window.location.href;
+
+       if (url.search("products/") >= 0) {
+         $('h1.title:contains("(Wholesale)")').closest("#content").addClass("wholesale-hd"); // Hide wholesale items for all users who type in direct url to products. Enter product URL's in above line
+       } 
+       {% if customer.tags contains 'Wholesale' %}
+           $("#product").removeClass("wholesale-hd"); // Show wholesale products for logged in wholesale customers.
+       {% endif %}  
+
+
+      });
+
+  </script>
