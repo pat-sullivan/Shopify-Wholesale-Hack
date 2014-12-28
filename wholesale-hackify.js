20ec5352a2c45c2a7bf3ed47dc493e35be714f4b
Patrick Sullivan <pat@optiprintdesign.com>
2014-12-28 00:10:13 -0500

    Update wholesale-hackify.js

--- wholesale-hackify.js
+++ wholesale-hackify.js
@@ -9,7 +9,7 @@
           var url = window.location.href;
              if (url.search("collections/all") >= 0) { // Will only execute the following code if the user is on the main collection page
                $('.title:contains("YOUR WHOLESALE PRODUCT TITLE")').closest(".product").addClass("clearit");
-           }; // The above code adds a clearing class to the parent product div that matches the text string in "contains"
+           }; // The above code adds a clearing class to the parent product div that matches the text string in "contains". It may not be necessary for your code.
         {% endif %} 
         
        var url = window.location.href;
